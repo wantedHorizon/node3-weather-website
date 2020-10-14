bd012388b29595850524154d279e36b7c456a418
WantedHorizon <eliran.amzalag@gmail.com>
2020-10-14 12:51:18 +0300

    fix app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,16 @@
 const request = require('request')
-
+const key ="56d4b4b60757133259db11d2eef911e6";
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
-
+    const url = `http://api.weatherstack.com/current?access_key=${key}&query=${latitude},${longitude}&units=m`
+    
     request({ url, json: true }, (error, { body }) => {
+        console.log(body);
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. It feels like '+ body.current.feelslike )
         }
     })
 }
